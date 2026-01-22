ec5428c9460b18e0400e0e131816dfa171418140
Guayanay Lema Erika Vannesa <vannesaguayanay12@gmail.com>
2026-01-22 07:55:59 -0500

    Pruebas Implementadas

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,5 +1,7 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class SubscriptionServiceTest {
@@ -7,5 +9,27 @@ public class SubscriptionServiceTest {
 	@Test
 	public void smokeTest() {}
 
-	
+	@Test
+	void addSubscriber_nullUser_throwsException() {
+    SubscriptionService service = new SubscriptionService();
+	assertThrows(
+        SubscriptionService.NullUserException.class,
+        () -> service.addSubscriber(null)
+    );
+}
+
+@Test
+void addSubscriber_existingUser_throwsException() throws Exception {
+    SubscriptionService service = new SubscriptionService();
+    User user = new User();
+
+    service.addSubscriber(user);
+
+    assertThrows(
+        SubscriptionService.ExistingUserException.class,
+        () -> service.addSubscriber(user)
+    );
+}
+
+
 }
