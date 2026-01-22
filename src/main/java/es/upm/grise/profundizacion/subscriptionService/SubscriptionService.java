5d071b6ca7ece16d5e23776a08b7e43c0d6b7f54
Jhosue-cmd <jdguayasamin2@espe.edu.ec>
2026-01-22 07:57:04 -0500

    Implementacion del codigo

--- src/main/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionService.java
+++ src/main/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionService.java
@@ -1,29 +1,43 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class SubscriptionService {
 
-	private Collection <User> subscribers;
-	
-	/*
-	 * Constructor
-	 */
-	public SubscriptionService() {}
-	
-	
+	private Collection<User> subscribers;
+
+	public SubscriptionService() {
+		this.subscribers = new ArrayList<>();
+	}
 
 	/*
 	 * Method to code / test
 	 */
-	public void addSubscriber(User user) {}
+	public void addSubscriber(User user) throws NullUserException, ExistingUserException, LocalUserDoesNotHaveNullEMailException {
+		
+		if (user == null) {
+			throw new NullUserException();
+		}
+		
+		
+		if (subscribers.contains(user)) {
+			throw new ExistingUserException();
+		}
+		
+		
+		if (user.getDelivery() == Delivery.LOCAL && user.getEmail() != null) {
+			throw new LocalUserDoesNotHaveNullEMailException();
+		}
+		
+		
+		subscribers.add(user);
+	}
 	
 	
 	
 	
-	/*
-	 * Other setters & getters
-	 */
+	
 	public Collection <User> getSubscribers() {
 		
 		return subscribers;
