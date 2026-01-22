317807552f32d753031c4b31d3e77eed5d67d0ef
bsgudino1416 <bsgudino@espe.edu.ec>
2026-01-22 07:55:41 -0500

    Parte-a2-Gudino

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,10 +1,54 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SubscriptionServiceTest {
-	
+
 	@Test
-	public void smokeTest() {}
-	
+	public void smokeTest() {
+		SubscriptionService service = new SubscriptionService();
+		assertNotNull(service);
+	}
+
+	@Test
+	public void addSubscriber_ok() {
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		user.setDelivery(Delivery.DO_NOT_DELIVER);
+		user.setEmail("test@test.com");
+
+		service.addSubscriber(user);
+
+		assertEquals(1, service.getSubscribers().size());
+	}
+
+	@Test
+	public void addSubscriber_nullUser_exception() {
+		SubscriptionService service = new SubscriptionService();
+		assertThrows(NullUserException.class, () -> service.addSubscriber(null));
+	}
+
+	@Test
+	public void addSubscriber_existingUser_exception() {
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		user.setDelivery(Delivery.DO_NOT_DELIVER);
+
+		service.addSubscriber(user);
+
+		assertThrows(ExistingUserException.class, () -> service.addSubscriber(user));
+	}
+
+	@Test
+	public void addSubscriber_localWithEmail_exception() {
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		user.setDelivery(Delivery.LOCAL);
+		user.setEmail("local@test.com");
+
+		assertThrows(LocalUserDoesNotHaveNullEMailException.class,
+				() -> service.addSubscriber(user));
+	}
 }
+
