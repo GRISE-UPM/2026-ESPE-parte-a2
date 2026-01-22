26138c8f63eb383c3e94afe5f6ab493d0286bbe9
saraycanarteg <sacanarte@espe.edu.ec>
2026-01-22 09:33:52 -0500

    Implementación método addSubscriber

--- src/main/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionService.java
+++ src/main/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionService.java
@@ -16,7 +16,22 @@ public class SubscriptionService {
 	/*
 	 * Method to code / test
 	 */
-	public void addSubscriber(User user) {}
+	public void addSubscriber(User user) {
+
+        if (user == null) {
+            throw new NullUserException();
+        }
+
+        if (subscribers.contains(user)) {
+            throw new ExistingUserException();
+        }
+
+		if (user.getDelivery() == Delivery.LOCAL && user.getEmail() != null) {
+			throw new LocalUserDoesNotHaveNullEMailException();
+		}
+
+        subscribers.add(user);
+    }
 	
 	
 	
